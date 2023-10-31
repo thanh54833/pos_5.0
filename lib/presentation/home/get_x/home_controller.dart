1aaf1393b827ea99136abb5ebaac99aac540a6cd
phamthanh <phamhoaithanh32@gmail.com>
2023-10-31 09:30:52 +0700

    +push code

--- lib/presentation/home/get_x/home_controller.dart
+++ lib/presentation/home/get_x/home_controller.dart
@@ -1,7 +1,7 @@
 import 'package:injectable/injectable.dart';
 import 'package:pos_5/test/injectable/cc_getx/cc_getx.dart';
 
-@singleton
+@injectable
 class HomeController extends ViewController {
 
 }
