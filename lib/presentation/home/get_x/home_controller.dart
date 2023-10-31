6cb36fd35e0c79b26ccc18d70ec3a657f7446400
phamthanh <phamhoaithanh32@gmail.com>
2023-10-31 09:38:12 +0700

    +push code

--- lib/presentation/home/get_x/home_controller.dart
+++ lib/presentation/home/get_x/home_controller.dart
@@ -2,7 +2,11 @@ import 'package:injectable/injectable.dart';
 import 'package:pos_5/test/injectable/cc_getx/cc_getx.dart';
 
 @injectable
+class ServiceA {}
+
+@injectable
 class HomeController extends ViewController {
+  ServiceA serviceA;
 
-
+  HomeController({required this.serviceA}) : super();
 }
